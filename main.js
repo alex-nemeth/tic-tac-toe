214f4087a1486d0d2933cc7d6bd638a5c7f199d9
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-03-03 15:04:02 +0100

    game state checking, reset button added

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const boxes = [];
 let mark = "X";
+let gameState = 0;
+let amountOfMoves = 0;
 
 boxes.push((boxOne = document.querySelector(".one")));
 boxes.push((boxTwo = document.querySelector(".two")));
@@ -17,16 +19,23 @@ function assignPlayer(mark) {
 }
 
 function move(index) {
-    boxes[index].textContent = mark;
-    checkGame(mark);
-    if (mark === "X") mark = "O";
-    else if (mark === "O") mark = "X";
+    if (gameState === 0) {
+        boxes[index].textContent = mark;
+        amountOfMoves++;
+        checkGame(mark);
+        if (mark === "X") mark = "O";
+        else if (mark === "O") mark = "X";
+    }
 }
 
 function checkGame(mark) {
     checkColumns(mark);
     checkRows(mark);
     checkDiagonals(mark);
+    if (amountOfMoves === 9) {
+        alert("It's a draw!");
+        gameState = 1;
+    }
 }
 
 function checkColumns(mark) {
@@ -34,20 +43,24 @@ function checkColumns(mark) {
         boxes[0].textContent === mark &&
         boxes[3].textContent === mark &&
         boxes[6].textContent === mark
-    )
+    ) {
         alert(`${assignPlayer(mark)} wins!`);
-    else if (
+        gameState = 1;
+    } else if (
         boxes[1].textContent === mark &&
         boxes[4].textContent === mark &&
         boxes[7].textContent === mark
-    )
+    ) {
         alert(`${assignPlayer(mark)} wins!`);
-    else if (
+        gameState = 1;
+    } else if (
         boxes[2].textContent === mark &&
         boxes[5].textContent === mark &&
         boxes[8].textContent === mark
-    )
+    ) {
         alert(`${assignPlayer(mark)} wins!`);
+        gameState = 1;
+    }
 }
 
 function checkRows(mark) {
@@ -55,20 +68,24 @@ function checkRows(mark) {
         boxes[0].textContent === mark &&
         boxes[1].textContent === mark &&
         boxes[2].textContent === mark
-    )
+    ) {
         alert(`${assignPlayer(mark)} wins!`);
-    else if (
+        gameState = 1;
+    } else if (
         boxes[3].textContent === mark &&
         boxes[4].textContent === mark &&
         boxes[5].textContent === mark
-    )
+    ) {
         alert(`${assignPlayer(mark)} wins!`);
-    else if (
+        gameState = 1;
+    } else if (
         boxes[6].textContent === mark &&
         boxes[7].textContent === mark &&
         boxes[8].textContent === mark
-    )
+    ) {
         alert(`${assignPlayer(mark)} wins!`);
+        gameState = 1;
+    }
 }
 
 function checkDiagonals() {
@@ -76,12 +93,24 @@ function checkDiagonals() {
         boxes[0].textContent === mark &&
         boxes[4].textContent === mark &&
         boxes[8].textContent === mark
-    )
+    ) {
         alert(`${assignPlayer(mark)} wins!`);
-    else if (
+        gameState = 1;
+    } else if (
         boxes[2].textContent === mark &&
         boxes[4].textContent === mark &&
         boxes[6].textContent === mark
-    )
+    ) {
         alert(`${assignPlayer(mark)} wins!`);
+        gameState = 1;
+    }
+}
+
+function reset() {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].textContent = "";
+    }
+    gameState = 0;
+    amountOfMoves = 0;
+    mark = "X";
 }
